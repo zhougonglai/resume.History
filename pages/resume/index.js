adda9b3526b15e425f8e613111052ad60dd74193
zhougonglai <zhou_gong_lai@qq.com>
2019-11-20 00:09:04 +0800

    type: update

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -1,24 +1,487 @@
-import dynamic from 'next/dynamic';
 import MDCLayout from '../../components/layout/MDC.layout';
-import CircleLoading from '../../components/circle-loading';
+import { Grid, Row, Cell } from '@material/react-layout-grid';
+import MaterialIcon from '@material/react-material-icon';
+import Router from 'next/router';
+import { ChipSet, Chip } from '@material/react-chips';
+import Card, { CardPrimaryContent, CardMedia } from '@material/react-card';
+import List, {
+	ListItem,
+	ListItemText,
+	ListItemMeta,
+	ListItemGraphic,
+} from '@material/react-list';
+import IconButton from '@material/react-icon-button';
+import Button from '@material/react-button';
+import {
+	Headline6,
+	Subtitle1,
+	Subtitle2,
+	Caption,
+} from '@material/react-typography';
+import Dialog, { DialogTitle, DialogContent } from '@material/react-dialog';
 import './index.scss';
-
-const Container = dynamic(() => import('../../components/resume'), {
-	loading: (
-		<div className='loading-fit'>
-			<CircleLoading />
-		</div>
-	),
-	ssr: false,
-});
 
 export default class Resume extends React.Component {
 	static Layout = MDCLayout;
+	state = {
+		open: false,
+		recent: false,
+	};
+
+	constructor(props) {
+		super(props);
+		this.dialogRef = React.createRef();
+	}
+
+	selectItem = index => {
+		switch (index) {
+			case 0:
+				this.setState({
+					open: !this.state.open,
+				});
+				break;
+			case 1:
+				window.open(
+					`mailto:zhou_gong_lai@qq.com?subject=[招聘]邀请投递:前端&body=`,
+				);
+				break;
+			case 2:
+				window.open(`https://www.amap.com/search?query=武汉市`);
+				break;
+		}
+	};
+
+	switchCase = target => {
+		if (target) {
+			Router.push(`/resume/projects#${target}`).then(() => {
+				Router.events.emit('push', {
+					activeIndex: 1,
+					type: '项目经历',
+				});
+			});
+		} else {
+			this.props.giveToast({
+				message: '本作品没有亮点',
+				open: true,
+			});
+		}
+	};
+
+	togglerRecent = () => {
+		this.setState({
+			recent: !this.state.recent,
+		});
+	};
 
 	render() {
 		return (
 			<div className='container'>
-				<Container />
+				<Dialog open={this.state.open}>
+					<IconButton
+						className='dialog-close'
+						onClick={() => this.selectItem(0)}>
+						<MaterialIcon icon='close' />
+					</IconButton>
+					<DialogTitle>微信二维码</DialogTitle>
+					<DialogContent>
+						<img className='qrcode' src='/WechatIMG35.jpeg' alt='qrcode' />
+					</DialogContent>
+				</Dialog>
+				<Dialog open={this.state.recent}>
+					<IconButton
+						className='dialog-close'
+						onClick={() => this.togglerRecent()}>
+						<MaterialIcon icon='close' />
+					</IconButton>
+					<DialogTitle>近况</DialogTitle>
+					<DialogContent>
+						<img className='recent' src='/WechatIMG36.jpeg' alt='recent' />
+					</DialogContent>
+				</Dialog>
+				<Grid>
+					<Row>
+						{/* 基本信息 */}
+						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
+							<div className='subtitle'>
+								<Subtitle1>基本信息</Subtitle1>
+							</div>
+							<Card
+								onClick={() => window.open('https://github.com/zhougonglai')}>
+								<CardPrimaryContent className='card__primary-action'>
+									<CardMedia
+										square
+										className='card__media'
+										imageUrl='/WechatIMG37.jpeg'
+									/>
+									<div className='card__primary'>
+										<Headline6 className='card__title'>
+											周公来
+											<svg className='icon ml-1' aria-hidden='true'>
+												<use xlinkHref='#icon-Man'></use>
+											</svg>
+										</Headline6>
+										<Subtitle2 className='card__subtitle'>
+											前端开发工程师
+										</Subtitle2>
+										<Caption>4 年工作经验 / 大专 / 27岁</Caption>
+										<MaterialIcon
+											className='fit-content'
+											aria-label='arrow_forward'
+											hasRipple
+											icon='arrow_forward'
+										/>
+									</div>
+								</CardPrimaryContent>
+							</Card>
+						</Cell>
+						{/* 联系我 */}
+						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
+							<div className='subtitle'>
+								<Subtitle1>联系我</Subtitle1>
+							</div>
+							<div className='card'>
+								<List handleSelect={active => this.selectItem(active)}>
+									<ListItem>
+										<ListItemGraphic
+											graphic={
+												<svg className='icon' aria-hidden='true'>
+													<use xlinkHref='#icon-weichat'></use>
+												</svg>
+											}
+										/>
+										<ListItemText primaryText='微信' />
+										<ListItemMeta
+											meta={
+												<MaterialIcon
+													className='fit-content'
+													aria-label='launch'
+													hasRipple
+													icon='launch'
+												/>
+											}
+										/>
+									</ListItem>
+									<ListItem>
+										<ListItemGraphic
+											graphic={
+												<svg className='icon' aria-hidden='true'>
+													<use xlinkHref='#icon-mail'></use>
+												</svg>
+											}
+										/>
+										<ListItemText primaryText='zhou_gong_lai@qq.com' />
+										<ListItemMeta meta='email' />
+									</ListItem>
+
+									<ListItem>
+										<ListItemGraphic
+											graphic={
+												<svg className='icon' aria-hidden='true'>
+													<use xlinkHref='#icon-Geo-fence'></use>
+												</svg>
+											}
+										/>
+										<ListItemText primaryText='武汉地区' />
+										<ListItemMeta
+											meta={
+												<MaterialIcon
+													className='fit-content'
+													aria-label='arrow_forward'
+													hasRipple
+													icon='my_location'
+												/>
+											}
+										/>
+									</ListItem>
+								</List>
+							</div>
+						</Cell>
+						{/* 核心技能 */}
+						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
+							<div className='subtitle'>
+								<Subtitle1>核心技能</Subtitle1>
+							</div>
+							<List>
+								<ListItem>
+									<ListItemGraphic
+										graphic={<img src='/logo.png' alt='vue' />}
+									/>
+									<ListItemText primaryText='vuex . vue-router . vue-cli . Nuxt' />
+								</ListItem>
+								<ListItem>
+									<ListItemGraphic
+										graphic={<img src='/download.svg' alt='react' />}
+									/>
+									<ListItemText primaryText='redux . react-router . umijs . Next . RN' />
+								</ListItem>
+								<ListItem>
+									<ListItemGraphic
+										graphic={<img src='/angular.svg' alt='angular' />}
+									/>
+									<ListItemText primaryText='@ngrx/store . ionic . flutter' />
+								</ListItem>
+								<ListItem>
+									<ListItemGraphic
+										graphic={<img src='/node.png' alt='NodeJs' />}
+									/>
+									<ListItemText primaryText='express . koa . egg . GraphQL . Nest' />
+								</ListItem>
+								<ListItem>
+									<ListItemGraphic
+										graphic={
+											<MaterialIcon
+												className='fit-content'
+												aria-label='CI'
+												hasRipple
+												icon='account_tree'
+											/>
+										}
+									/>
+									<ListItemText primaryText='Docker . Nginx .' />
+								</ListItem>
+							</List>
+						</Cell>
+						{/* 自我描述 */}
+						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
+							<div className='subtitle'>
+								<Subtitle1>自我描述</Subtitle1>
+								<div className='fill'></div>
+								<Button
+									trailingIcon={<MaterialIcon icon='info' />}
+									onClick={() => this.togglerRecent()}>
+									秃头警告
+								</Button>
+							</div>
+							<p className='indent'>
+								长期作战一线的码农.4年时间参与了大大小小数十个项目.项目涉及PC.Mobile.
+								微信平台.国外平台等各类项目.能够独立编码.主动尝试各种项目优化方案.习惯性快速学习.
+								热衷学习热门技术.练习相关解决方案.
+								<br />
+								<small>
+									tip:
+									早期常常熬夜加班到天亮.对身心造成了极大的影响.目前不太能接受996或连续性加班.
+								</small>
+							</p>
+						</Cell>
+						{/* 工作经历 */}
+						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
+							<div className='subtitle'>
+								<Subtitle1>工作经历</Subtitle1>
+							</div>
+							<div className='cell-list'>
+								<div className='cell-item'>
+									<div className='item-content'>
+										<img src='/logo_default.png' className='avatar' />
+										<div className='item-info'>
+											<strong>武汉无限未来科技有限公司 (软件服务)</strong>
+											<p>研发部 / 前端开发工程师</p>
+										</div>
+										<div className='time-line'>
+											<i>2019.9 ~ 2019.11</i>
+										</div>
+									</div>
+									<div className='item-container'>
+										<div className='chips'>
+											参与项目:
+											<ChipSet>
+												<Chip
+													label='微信-天马报告 [ 手机页 ]'
+													handleInteraction={() => this.switchCase('pegasus')}
+												/>
+												<Chip
+													label='电商App孵化 [ 双平台APP+手机页 . 电商 ]'
+													handleInteraction={() =>
+														this.switchCase('donew-store')
+													}
+												/>
+												<Chip
+													label='电商页面孵化 [ 跨终端企业站 . 响应式电商 ]'
+													handleInteraction={() => this.switchCase('fpw')}
+												/>
+											</ChipSet>
+										</div>
+									</div>
+								</div>
+								{/* 上海雷神 */}
+								<div className='cell-item'>
+									<div className='item-content'>
+										<img src='/leigod.jpg' className='avatar' />
+										<div className='item-info'>
+											<strong>上海雷神网络科技有限公司 (网游加速器)</strong>
+											<p>NN语音研发部 / 前端开发工程师</p>
+										</div>
+										<div className='time-line'>
+											<i>2019.7 ~ 2019.9</i>
+										</div>
+									</div>
+									<div className='item-container'>
+										<div className='chips'>
+											参与项目:
+											<ChipSet>
+												<Chip
+													label='微信-NN约玩 [ 手机页 ]'
+													handleInteraction={() => this.switchCase('nnplayer')}
+												/>
+											</ChipSet>
+										</div>
+									</div>
+								</div>
+								{/* 兰迪 */}
+								<div className='cell-item'>
+									<div className='item-content'>
+										<img src='/abc360.png' className='avatar' />
+										<div className='item-info'>
+											<strong>杭州旦悦科技 (少儿教育)</strong>
+											<p>兰迪2组(教师) / 前端开发工程师</p>
+										</div>
+										<div className='time-line'>
+											<i>2017.10 ~ 2019.3</i>
+										</div>
+									</div>
+									<div className='item-container'>
+										<div className='chips'>
+											参与项目:
+											<ChipSet>
+												<Chip
+													label='TPlus - [ 面向海外教师 ]'
+													handleInteraction={() => this.switchCase('tp2')}
+												/>
+												<Chip
+													label='EPlus - [ 业务运营项目 ]'
+													handleInteraction={() => this.switchCase()}
+												/>
+												<Chip
+													label='微信-作业 - [ 学生课后作业 ]'
+													handleInteraction={() => this.switchCase('homework')}
+												/>
+											</ChipSet>
+										</div>
+									</div>
+								</div>
+
+								<div className='cell-item'>
+									<div className='item-content'>
+										<img src='/utang.png' className='avatar' />
+										<div className='item-info'>
+											<strong>成都悠唐网络科技有限公司 (软件服务)</strong>
+											<p>开发组 / 前端开发工程师</p>
+										</div>
+										<div className='time-line'>
+											<i>2017.03 ~ 2017.06</i>
+										</div>
+									</div>
+									<div className='item-container'>
+										<div className='chips'>
+											参与项目:
+											<ChipSet>
+												<Chip
+													label='U信 - [ API service ]'
+													handleInteraction={() => this.switchCase()}
+												/>
+												<Chip
+													label='5km - [ 阅读社交 ]'
+													handleInteraction={() => this.switchCase()}
+												/>
+											</ChipSet>
+										</div>
+									</div>
+								</div>
+
+								<div className='cell-item'>
+									<div className='item-content'>
+										<img src='/yifudai.png' className='avatar' />
+										<div className='item-info'>
+											<strong>亿富贷商务顾问科技有限公司 (P2P金融)</strong>
+											<p>技术部 / 程序员</p>
+										</div>
+										<div className='time-line'>
+											<i>2015.06 ~ 2017.02</i>
+										</div>
+									</div>
+									<div className='item-container'>
+										<div className='chips'>
+											参与项目:
+											<ChipSet>
+												<Chip
+													label='企业官网'
+													handleInteraction={() => this.switchCase()}
+												/>
+												<Chip
+													label='微信服务'
+													handleInteraction={() => this.switchCase()}
+												/>
+												<Chip
+													label='混合应用'
+													handleInteraction={() => this.switchCase()}
+												/>
+											</ChipSet>
+										</div>
+									</div>
+								</div>
+							</div>
+						</Cell>
+						{/* 教育经历 */}
+						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
+							<div className='subtitle'>
+								<Subtitle1>教育经历</Subtitle1>
+							</div>
+
+							<div className='cell-list'>
+								<div className='cell-item'>
+									<div className='item-content'>
+										<img src='/bglg.jpg' className='avatar' />
+										<div className='item-info'>
+											<strong>北京理工大学</strong>
+											<p>大专 / 计算机应用</p>
+										</div>
+										<div className='time-line'>
+											<i>2018 ~ 2020</i>
+										</div>
+									</div>
+								</div>
+
+								<div className='cell-item'>
+									<div className='item-content'>
+										<img src='/udacity-cn.svg' className='avatar' />
+										<div className='item-info'>
+											<strong>优达学城</strong>
+											<p>网教 / React</p>
+										</div>
+										<div className='time-line'>
+											<i>2018</i>
+										</div>
+									</div>
+								</div>
+
+								<div className='cell-item'>
+									<div className='item-content'>
+										<img src='/tanzhou_logo.png' className='avatar' />
+										<div className='item-info'>
+											<strong>潭州学院</strong>
+											<p>网教 / Java</p>
+										</div>
+										<div className='time-line'>
+											<i>2014</i>
+										</div>
+									</div>
+								</div>
+
+								<div className='cell-item'>
+									<div className='item-content'>
+										<img src='/jianghan.jpg' className='avatar' />
+										<div className='item-info'>
+											<strong>江汉大学</strong>
+											<p>自考 / 电子信息工程(辍学)</p>
+										</div>
+										<div className='time-line'>
+											<i>2011 ~ 2014</i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</Cell>
+					</Row>
+				</Grid>
 			</div>
 		);
 	}
--- pages/resume/skill.js
+++ pages/resume/skill.js
@@ -10,113 +10,75 @@ export default class Skill extends React.Component {
 		return (
 			<div className='skill-container'>
 				<Grid>
-					<Headline5 className='title mb-2'>前端技能</Headline5>
+					<Headline5 className='title'>前端技能</Headline5>
 					<Row>
 						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
-							<div className='flex justify-center'>
-								<CircleProcess title='Vue' label='熟练' process={75}>
-									<ul>
-										<li>多场景应用</li>
-										<li>SSR - Nuxt</li>
-										<li>Vuex,vue-router</li>
-										<li>cli@3</li>
-									</ul>
-								</CircleProcess>
-							</div>
-							<div className='alert mt-2'>
+							<CircleProcess title='Vue' label='熟练' process={75}>
+								<ul>
+									<li>多场景应用</li>
+									<li>SSR - Nuxt</li>
+									<li>Vuex,vue-router</li>
+									<li>cli@3</li>
+								</ul>
+							</CircleProcess>
+							<div className='alert'>
 								tip: 自2017年初起从
 								<code className='text-danger'>angularJs</code>
 								转战<code className='text-danger'>Vue</code>
 								时间长达2年有余.在"杭州旦悦科技"团队中长期使用Vue开发各类项目.
-								其中包括企业级客户端-兰迪教师客户端.
+								其中包括企业级客户端-兰迪教师.
 							</div>
 						</Cell>
 						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
-							<div className='flex justify-center'>
-								<CircleProcess title='React' label='较熟练' process={65}>
-									<ul>
-										<li>精品小巧</li>
-										<li>SSR</li>
-										<li>RN应用</li>
-									</ul>
-								</CircleProcess>
-							</div>
-							<div className='alert mt-2'>
-								tip: 最初从<code className='text-danger'>0.10.x</code>
-								版本开始入门react.配合当时兴起的babel. 在2018年初在
-								<code className='text-danger'>Udacity</code>
-								完成React纳米专业课程.于2018年下半年.
-								<code className='text-danger'>Vue圈RFC稿</code>
-								事件.开始在团队推广React体系.得益于
-								<code className='text-danger'>IceWork</code>
-								在2019年初快速启动多个项目
-							</div>
+							<CircleProcess title='React' label='较熟练' process={65}>
+								<ul>
+									<li>精品小巧</li>
+									<li>SSR</li>
+									<li>RN应用</li>
+								</ul>
+							</CircleProcess>
 						</Cell>
 						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
-							<div className='flex justify-center'>
-								<CircleProcess title='Angular' label='较熟练' process={60}>
-									<ul>
-										<li>企业级应用</li>
-										<li>ionic混合应用</li>
-									</ul>
-								</CircleProcess>
-							</div>
-							<div className='alert mt-2'>
-								tip:接触前端最开始是从
-								<code className='text-danger'>angularJs</code>开始的. 从
-								<code className='text-danger'>angular TS</code>
-								开始.大部分企业团队开始使用Vue和React.
-								一直当作兴趣爱好来学习.最贴近企业业务的就是
-								<code className='text-danger'>Ionic</code>.
-							</div>
+							<CircleProcess title='Angular' label='较熟练' process={60}>
+								<ul>
+									<li>企业级应用</li>
+									<li>hybird混合应用 - ionic</li>
+								</ul>
+							</CircleProcess>
 						</Cell>
 						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
-							<div className='flex justify-center'>
-								<CircleProcess title='Flutter' label='上手' process={42}>
-									<ul>
-										<li>Material组件.布局</li>
-										<li>请求 [http, dio]</li>
-										<li>状态管理</li>
-									</ul>
-								</CircleProcess>
-							</div>
-							<div className='alert mt-2'>
-								tip:入门级,会布局.对接请求.组织组件.管理状态.
-							</div>
+							<CircleProcess title='Flutter' label='上手' process={45}>
+								<ul>
+									<li>Material组件.布局</li>
+									<li>请求 [http, dio]</li>
+									<li>状态管理</li>
+								</ul>
+							</CircleProcess>
 						</Cell>
 					</Row>
-					<Headline5 className='title my-2'>后端相关</Headline5>
+					<Headline5 className='title'>后端相关</Headline5>
 					<Row>
 						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
-							<div className='flex justify-center'>
-								<CircleProcess title='NodeJs' label='上手' process={50}>
-									<ul>
-										<li>SSR渲染</li>
-										<li>GraphQL/Apollo</li>
-										<li>企业中台 - egg</li>
-										<li>serverless</li>
-										<li>soket.io</li>
-									</ul>
-								</CircleProcess>
-							</div>
-							<div className='alert mt-2'>
-								tip:构建非主干<code className='text-danger'>中台/后台</code>
-								服务.GraphQL/Apollo配合serverless更好的输出API.
-								于雷神网络科技有限公司接触WebRTC.
-							</div>
+							<CircleProcess title='NodeJs' label='上手' process={50}>
+								<ul>
+									<li>SSR渲染</li>
+									<li>GraphQL/Apollo</li>
+									<li>企业中台 - egg</li>
+									<li>serverless</li>
+									<li>soket.io</li>
+								</ul>
+							</CircleProcess>
 						</Cell>
 					</Row>
-					<Headline5 className='title mb-2'>部署相关</Headline5>
+					<Headline5 className='title'>部署相关</Headline5>
 					<Row>
 						<Cell phoneColumns={4} tabletColumns={4} desktopColumns={4}>
-							<div className='flex justify-center'>
-								<CircleProcess title='Docker' label='上手' process={50}>
-									<ul>
-										<li>Node容器</li>
-										<li>Nginx容器</li>
-									</ul>
-								</CircleProcess>
-							</div>
+							<CircleProcess title='Docker' label='上手' process={50}>
+								<ul>
+									<li>Node容器</li>
+									<li>Nginx容器</li>
+								</ul>
+							</CircleProcess>
 						</Cell>
 					</Row>
 				</Grid>
