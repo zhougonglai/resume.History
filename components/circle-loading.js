8fc7afbffa38fb38b8f6293d77a7400235c9a086
zhougonglai <zhou_gong_lai@qq.com>
2019-11-19 18:58:56 +0800

    type: update

---
+++ components/circle-loading.js
@@ -0,0 +1,9 @@
+import './circle-loading.scss';
+
+export default () => {
+	return (
+		<svg id='load' x='0px' y='0px' viewBox='0 0 150 150'>
+			<circle id='loading-inner' cx='75' cy='75' r='60' />
+		</svg>
+	);
+};
