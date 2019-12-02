aedb1c9e91165657f7bad1c74a38e8d1f7cb17ef
zhougonglai <zhou_gong_lai@qq.com>
2019-12-03 07:24:45 +0800

    type: update

--- components/hovercard/index.js
+++ components/hovercard/index.js
@@ -1,5 +1,5 @@
 import './index.scss';
 
 export default ({ children, className }) => {
-	return <div className={['card', className].join(' ')}>{children}</div>;
+	return <div className={['hover-card', className].join(' ')}>{children}</div>;
 };
